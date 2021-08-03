2ef205a6b4a0350887ecd0e831fc39e36e0d4765
amnesiac09 <alexnaneishvili@gmail.com>
2021-08-04 00:58:02 +0400

    cm

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,5 +1,41 @@
 package user;
 
 public class User {
-    //todo
+
+    private String username;
+    private String password;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private boolean isAdmin;
+
+    public User(String username, String password, int id, String firstName, String lastName, boolean isAdmin){
+        this.username = username;
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
 }
