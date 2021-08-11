c88136504b7d781758dc36957d8b1fe985cdc185
AkakiZZ <zghuladze16@gmail.com>
2021-08-12 00:36:45 +0400

    added database functionality

---
+++ reset.sql
@@ -0,0 +1,125 @@
+use quizwebsite_db;
+
+drop table standard_unordered_answers;
+drop table standard_unordered_questions;
+drop table multiple_choice_unordered_answers;
+drop table multiple_choice_unordered_questions;
+drop table picture_unordered_answers;
+drop table picture_unordered_questions;
+drop table multiple_answer_unordered_answers;
+drop table multiple_answer_unordered_questions;
+drop table quiz_history;
+drop table quizzes;
+drop table friend_requests;
+drop table friendship;
+drop table messages;
+drop table users;
+
+create table users (
+                       id int primary key auto_increment,
+                       username varchar(50),
+                       hashed_password varchar(200),
+                       is_admin boolean,
+                       first_name varchar(30),
+                       last_name varchar(30)
+);
+
+create table messages(
+                         id int primary key auto_increment,
+                         from_user_id int not null,
+                         to_user_id int not null,
+                         message_text varchar(1000) not null,
+                         foreign key(from_user_id) references users(id) on delete cascade,
+                         foreign key(to_user_id) references users(id) on delete cascade
+);
+
+create table friend_requests(
+                                id int primary key auto_increment,
+                                from_user_id int not null,
+                                to_user_id int not null,
+                                foreign key(from_user_id) references users(id) on delete cascade,
+                                foreign key(to_user_id) references users(id) on delete cascade
+);
+
+create table friendship(
+                           id int primary key auto_increment,
+                           first_user_id int not null references users(id) on delete cascade,
+                           second_user_id int not null references users(id) on delete cascade
+);
+
+create table quizzes(
+                        id int primary key auto_increment,
+                        author int not null,
+                        quiz_name varchar(200) not null,
+                        is_random_order boolean not null,
+                        foreign key(author) references users(id) on delete cascade
+);
+
+create table quiz_history(
+                             id int primary key auto_increment,
+                             quiz_id int not null,
+                             user_id int not null,
+                             score double not null,
+                             attempt_time timestamp not null,
+                             foreign key(quiz_id) references quizzes(id) on delete cascade,
+                             foreign key(user_id) references users(id) on delete cascade
+);
+
+create table standard_unordered_questions(
+                                             id int primary key auto_increment,
+                                             question_text varchar(800) not null,
+                                             quiz_id int not null,
+                                             foreign key(quiz_id) references quizzes(id) on delete cascade
+);
+
+create table standard_unordered_answers (
+                                            id int primary key auto_increment,
+                                            answer_text varchar(200) not null,
+                                            question_id int not null,
+                                            foreign key(question_id) references standard_unordered_questions(id) on delete cascade
+);
+
+create table multiple_choice_unordered_questions (
+                                                     id int primary key auto_increment,
+                                                     question_text varchar(800) not null,
+                                                     quiz_id int not null,
+                                                     foreign key(quiz_id) references quizzes(id) on delete cascade
+);
+
+create table multiple_choice_unordered_answers (
+                                                   id int primary key auto_increment,
+                                                   answer_text varchar(200) not null,
+                                                   question_id int not null,
+                                                   foreign key(question_id) references multiple_choice_unordered_questions(id) on delete cascade,
+                                                   is_correct boolean not null
+);
+
+create table picture_unordered_questions(
+                                            id int primary key auto_increment,
+                                            question_text varchar(800) not null,
+                                            img_url varchar(1000) not null,
+                                            quiz_id int not null,
+                                            foreign key(quiz_id) references quizzes(id) on delete cascade
+);
+
+create table picture_unordered_answers (
+                                           id int primary key auto_increment,
+                                           answer_text varchar(200) not null,
+                                           question_id int not null,
+                                           foreign key(question_id) references picture_unordered_questions(id) on delete cascade
+);
+
+create table multiple_answer_unordered_questions (
+                                                     id int primary key auto_increment,
+                                                     question_text varchar(800),
+                                                     quiz_id int not null,
+                                                     numOfRequestedAnswers int not null,
+                                                     foreign key(quiz_id) references quizzes(id) on delete cascade
+);
+
+create table multiple_answer_unordered_answers (
+                                                   id int primary key auto_increment,
+                                                   answer_text varchar(200),
+                                                   question_id int not null,
+                                                   foreign key(question_id) references multiple_answer_unordered_questions(id) on delete cascade
+);
